55999c3751cfd647e821ba4f5303595a28d03107
FoxJefisto <foxjefisto@gmail.com>
2024-05-01 01:43:07 +0300

    script edit

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [ "$1" = "dev" ]; then
+  docker-compose -f docker-compose.dev.yml down
   docker-compose -f docker-compose.dev.yml --env-file dev.env up --build
 elif [ "$1" = "prod" ]; then
+  docker-compose -f docker-compose.prod.yml down 
   docker-compose -f docker-compose.prod.yml --env-file prod.env up --build
 else
   echo "Unknown environment: $1"
