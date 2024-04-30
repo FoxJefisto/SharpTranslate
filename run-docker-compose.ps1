12e4570d3f337fadb0d69709e5257f1cf4d5e834
FoxJefisto <foxjefisto@gmail.com>
2024-05-01 02:17:11 +0300

    update scripts

--- run-docker-compose.ps1
+++ run-docker-compose.ps1
@@ -1,10 +1,37 @@
+# Check if the environment variable is set to "dev" or "prod"
 param ($env)
 
 if ($env -eq "dev") {
-  docker-compose -f docker-compose.dev.yml --env-file dev.env up --build
+  docker-compose -f docker-compose.dev.yml down
+  docker-compose -f docker-compose.dev.yml --env-file dev.env up --build --detach
 } elseif ($env -eq "prod") {
-  docker-compose -f docker-compose.prod.yml --env-file prod.env up --build
+  docker-compose -f docker-compose.prod.yml down
+  docker-compose -f docker-compose.prod.yml --env-file prod.env up --build --detach
 } else {
   Write-Host "Unknown environment: $env"
-  Exit 1
+  exit 1
 }
+
+# Wait for the Postgres container to start
+$COUNT = 0
+$MAX_COUNT = 10
+Start-Sleep -s 10
+
+while ((docker container inspect -f '{{.State.Running}}' postgres) -ne "true") {
+  Write-Host "Waiting for container to start..."
+  Start-Sleep -s 10
+  $COUNT++
+  if ($COUNT -ge $MAX_COUNT) {
+    Write-Host "Timeout: container did not start within 3 minutes"
+    exit 1
+  }
+}
+
+# Run EF Core migrations
+Write-Host "Running EF Core migrations..."
+if ($env -eq "dev") {
+  dotnet ef database update --project SharpTranslate -- --environment Development
+} elseif ($env -eq "prod") {
+  dotnet ef database update --project SharpTranslate
+}
+Write-Host "EF Core migrations complete"
--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
+# Check if the environment variable is set to "dev" or "prod"
 if [ "$1" = "dev" ]; then
   docker-compose -f docker-compose.dev.yml down
-  docker-compose -f docker-compose.dev.yml --env-file dev.env up --build
+  docker-compose -f docker-compose.dev.yml --env-file dev.env up --build --detach
 elif [ "$1" = "prod" ]; then
   docker-compose -f docker-compose.prod.yml down 
-  docker-compose -f docker-compose.prod.yml --env-file prod.env up --build
+  docker-compose -f docker-compose.prod.yml --env-file prod.env up --build --detach
 else
   echo "Unknown environment: $1"
   exit 1
 fi
+
+# Wait for the Postgres container to start
+COUNT=0
+MAX_COUNT=10
+sleep 10
+
+while [ "$(docker container inspect -f '{{.State.Running}}' postgres)" != "true" ]; do
+  echo "Waiting for container to start..."
+  sleep 10
+  COUNT=$((COUNT + 1))
+  if [ $COUNT -ge $MAX_COUNT ]; then
+    echo "Timeout: container did not start within 3 minutes"
+    exit 1
+  fi
+done
+
+# Run EF Core migrations
+echo "Running EF Core migrations..."
+if [ "$1" = "dev" ]; then
+  dotnet ef database update --project SharpTranslate -- --environment Development
+elif [ "$1" = "prod" ]; then
+  dotnet ef database update --project SharpTranslate
+fi
+echo "EF Core migrations complete"
