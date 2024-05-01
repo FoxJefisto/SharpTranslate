2bd68bc55723224fc71f9676c7db434428317b57
FoxJefisto <foxjefisto@gmail.com>
2024-05-01 04:01:36 +0300

    fix script

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -33,11 +33,23 @@ echo "Postgres container is running"
 # Containers ps
 docker ps -a
 
+# Check if dotnet-ef is installed
+dotnet-ef --version
+
+if dotnet-ef --version
+then
+  echo "dotnet-ef is already installed"
+else
+  # Install dotnet-ef if it's not installed
+  echo "dotnet-ef is not installed. Installing..."
+  dotnet tool install --global dotnet-ef --version 6.0.29
+fi
+
 # Run EF Core migrations
 echo "Running EF Core migrations..."
 if [ "$1" = "dev" ]; then
-  dotnet ef database update --project SharpTranslate -- --environment Development
+  dotnet-ef database update --project SharpTranslate -- --environment Development
 elif [ "$1" = "prod" ]; then
-  dotnet ef database update --project SharpTranslate -- --environment Production
+  dotnet-ef database update --project SharpTranslate -- --environment Production
 fi
 echo "EF Core migrations complete"
